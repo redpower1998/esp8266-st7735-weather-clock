164bac972e566d283c43a9e08b3f76f57690debf
redpower <xuechangyu@hotmail.com>
2021-03-28 17:50:00 +0800

    add WeatherClock/configure.h.

---
+++ WeatherClock/configure.h
@@ -0,0 +1,21 @@
+#ifndef CONFIGURE_HH
+#define CONFIGURE_HH
+
+//SSID and Password of your WiFi router
+const char* ssid = "XXXXXXXXx";
+const char* password = "XXXXXXXXXXX";
+
+
+String APIKEY = "XXXXXXXXXXXXXXXXX"; //openweathermap.org
+String CityID = "XXXXXXX";  //ID not name
+
+const int timezone = 8;
+int dst = 0;
+
+// Pins
+#define TFT_CS     D1
+#define TFT_RST    D2
+#define TFT_RS     D3  // AO
+#define TFT_SDI    D7  
+
+#endif
